06387f60b91b9ef1de25acf94592be3cd6d804b8
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2022-04-11 13:44:51 +0200

    Mise en place d'une structure dédiée à la recherche de doonnées.

---
+++ src/fr/finn/donnees/DonneesAction.java
@@ -0,0 +1,13 @@
+package fr.finn.donnees;
+
+/**
+ * La classe Donnees actions permet de récolter les données de la bourse sur Internet afin de les retransmettre à l'utilisateur.
+ *
+ * L'API marketstack est utilisé.
+ */
+public class DonneesAction {
+
+    //Attributs
+    private final static String cleAccces = "15154d4d1a6057e69d6ac608cf1779a0"; //Clé d'accès à l'API
+
+}
